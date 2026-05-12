c34b4a2d87a96841dba2afe5517ce5d15c40bae0
dipu <dipu@localhost>
2026-05-12 18:50:04 +0200

    milestone1 update

--- milestone1/src/main.rs
+++ milestone1/src/main.rs
@@ -1,3 +1,29 @@
+
+// lecture video 12: concepts of lifetime, traits, and generics in Rust
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str, 
+    ann: T
+) -> &'a str 
+where
+    T: Display,  // this is a trait bound, it specifies that T must implement the Display trait
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest_with_an_announcement(string1.as_str(), string2, "Comparing two strings");
+    println!("The longest string is {}", result);
 }
