049877b7b2e4975332ed928637473483924b8145
dipu <dipu@localhost>
2026-05-19 21:11:18 +0200

    error printed to terminal and not output

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err|{
-        print!("Problem parsing arguments: {}", err);
+        eprint!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -20,7 +20,7 @@ fn main() {
 
 
     if let Err(e) =minigrep::run(config){
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
