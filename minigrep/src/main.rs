85f8f6b6fbac2eed0b523bd1062e63006da25b9b
dipu <dipu@localhost>
2026-05-19 20:17:57 +0200

    seperated config logic to struct imp

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,53 @@
 use std::env;
-
+use std::error::Error;
+use std::fs;
+use std::process;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    println!("Arguments: {:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        print!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+
+
+
+    if let Err(e) =run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
+fn run(config:Config)-> Result<(), Box<dyn Error>>{
+    let content = fs::read_to_string(config.filename)?;
+    // .expect("Something went wrong reading the file");
+    // println!("Arguments: {:?}", args);
+    println!("Content: \n{}", content);
+    
+    Ok(())
+
+}
+
+struct Config{
+    query: String,
+    filename: String
+}
+
+impl Config{
+    fn new(args:&[String]) -> Result<Config, &str>{
+        if args.len() < 3 {
+            return Err("Not enough arguments\n");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config{query, filename})
+    }
+}
+
