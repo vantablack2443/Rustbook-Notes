6845e30ece9b52a5c6ce5716b95da50eec4a79b5
dipu <dipu@localhost>
2026-05-21 02:24:04 +0200

    ran cargo fmt

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,9 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 // run function to read the content of the file and search for the query in the content
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
     // .expect("Something went wrong reading the file");
     // println!("Arguments: {:?}", args);
@@ -16,21 +16,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     } {
         println!("{}", line);
     }
-    
-    Ok(())
 
+    Ok(())
 }
 
 // Config struct to hold the query and filename
-pub struct Config{
+pub struct Config {
     pub query: String,
     pub filename: String,
-    pub case_sensitive: bool
+    pub case_sensitive: bool,
 }
 
 // implementation of the Config struct to create a new Config instance from the command line arguments
-impl Config{
-    pub fn new(args:&[String]) -> Result<Config, &str>{
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("Not enough arguments\n");
         }
@@ -39,69 +38,69 @@ impl Config{
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config{query, filename, case_sensitive})
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
-
 
 // function to search for the query in the content in a case sensitive way
 
-pub fn search_case_sensitive <'a>(query: &str, content: &'a str) -> Vec<&'a str>{
-
+pub fn search_case_sensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
-    for line in content.lines(){
-        if line.contains(query){
+    for line in content.lines() {
+        if line.contains(query) {
             results.push(line);
         }
     }
     results
 }
-
 
 // function to search for the query in the content in a case insensitive way
-pub fn search_case_insensitive <'a>(query: &str, content: &'a str) -> Vec<&'a str>{
-
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
-    for line in content.lines(){
-        if line.to_lowercase().contains(&query.to_lowercase()){
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
             results.push(line);
         }
     }
     results
 }
-
 
 // tests for the search functions
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn case_sensitive(){
+    fn case_sensitive() {
         let query = "help";
         let content = "\
 Rust:
 safe, fast, productive.
 Pick three.
 help me mister damu.";
-        assert_eq!(vec!["help me mister damu."], 
-        search_case_sensitive(query, content));
+        assert_eq!(
+            vec!["help me mister damu."],
+            search_case_sensitive(query, content)
+        );
     }
 
-
     #[test]
-    fn case_insensitive(){
+    fn case_insensitive() {
         let query = "DuCt";
         let content = "\
 Rust is DuCt:
 safe, fast, productive.
 Pick three.
 Duct tape.";
-        assert_eq!(vec!["Rust is DuCt:", "Duct tape.", "safe, fast, productive."].sort(),
-        search_case_insensitive(query, content).sort());
+        assert_eq!(
+            vec!["Rust is DuCt:", "Duct tape.", "safe, fast, productive."].sort(),
+            search_case_insensitive(query, content).sort()
+        );
     }
 }
-
-
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,26 +1,22 @@
 use std::env;
 
-use std::process;
 use minigrep::Config;
+use std::process;
 
 fn main() {
-
     // getting the arguments from command line
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = Config::new(&args).unwrap_or_else(|err| {
         eprint!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-
     println!("Searching for : {}", config.query);
     println!("In file : {}", config.filename);
 
-
-
     // reading the content of the file and searching for the query in the content
-    if let Err(e) =minigrep::run(config){
+    if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
